44fa6315faa1f9d2a266c9e94738199f72f881a8
csarnaim <csarnaimarton@gmail.com>
2020-04-08 19:55:52 +0200

    Final version of the lexer.

--- beadando.py
+++ beadando.py
@@ -1,8 +1,7 @@
+import re
+
 # domosi123{}}123domosi123:=(*)<>1domosi1
 bemenet = 'domosi123{}}123domosi123:=(**)<>1domosi1'
-
-
-import re
 
 azonosito = re.compile('[a-zA-Z]+[a-zA-Z0-9]*')
 konstans = re.compile('[0-9]+')
@@ -14,71 +13,61 @@ ertekadas3 = re.compile('>=')
 
 final = []
 
-for char in bemenet:
-    # print(bemenet)
+while (len(bemenet) != 0):
+    start = bemenet[0]
 
-    if str.isalpha(char):
+    if str.isalpha(start):
         if azonosito.match(bemenet):
             match = azonosito.match(bemenet)
             final.append("<azonosító>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif str.isnumeric(char):
+    elif str.isnumeric(start):
         if konstans.match(bemenet):
             match = konstans.match(bemenet)
             final.append("<konstans>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif char == '{':
+    elif start == '{':
         if kommentar1.match(bemenet):
             match = kommentar1.match(bemenet)
             final.append("<kommentár>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif char == '(':
-        print(kommentar2.match(bemenet))
+    elif start == '(':
         if kommentar2.match(bemenet):
             match = kommentar2.match(bemenet)
             final.append("<kommentár>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif char == ':':
+    elif start == ':':
         if ertekadas1.match(bemenet):
             match = ertekadas1.match(bemenet)
             final.append("<értékadás>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif char == '<':
+    elif start == '<':
         if ertekadas2.match(bemenet):
             match = ertekadas2.match(bemenet)
             final.append("<értékadás>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
-    elif char == '>':
+    elif start == '>':
         if ertekadas3.match(bemenet):
             match = ertekadas3.match(bemenet)
             final.append("<értékadás>")
             vag = match.span()
             bemenet = bemenet[vag[1]:]
-            print(bemenet)
 
     else:
-        # print("unkwown char: " + char)
-        final.append(char)
+        final.append(start)
         bemenet = bemenet[1:]
         continue
-
 
 print(final)
