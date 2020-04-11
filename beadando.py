5f57f59c341bcffa5c23cac4c5f604353e35ed11
csarnaim <csarnaimarton@gmail.com>
2020-04-11 22:00:18 +0200

    Final version of the lexer.

--- beadando.py
+++ beadando.py
@@ -1,7 +1,6 @@
 import re
 
-
-# bemenet = 'domosi123{}123domosi123:=(**)<>1domosi1'
+# bemenet = 'domosi123{}123domosi123:=(**valami*)<>1domosi1'
 
 
 class InvalidTokenError(Exception):
@@ -33,50 +32,98 @@ while (len(bemenet) != 0):
     if str.isalpha(start):
         if azonosito.match(bemenet):
             match = azonosito.match(bemenet)
-            final.append("<azonosító>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 2")
+                elif (i == vag[1] - 1):
+                    final.append("3")
+                else:
+                    final.append("2")
             bemenet = bemenet[vag[1]:]
 
     elif str.isnumeric(start):
         if konstans.match(bemenet):
             match = konstans.match(bemenet)
-            final.append("<konstans>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 4")
+                elif (i == vag[1] - 1):
+                    final.append("5")
+                else:
+                    final.append("4")
             bemenet = bemenet[vag[1]:]
 
     elif start == '{':
         if kommentar1.match(bemenet):
             match = kommentar1.match(bemenet)
-            final.append("<kommentár>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 6")
+                elif (i == vag[1] - 1):
+                    final.append("7")
+                else:
+                    final.append("6")
             bemenet = bemenet[vag[1]:]
 
     elif start == '(':
         if kommentar2.match(bemenet):
             match = kommentar2.match(bemenet)
-            final.append("<kommentár>")
             vag = match.span()
+            flag = 0
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 8")
+                elif (i == 1):
+                    final.append("9")
+                elif (i > 1 and char == "*"):
+                    final.append("10")
+                    flag = 1
+                elif (i == vag[1] - 1):
+                    final.append("11")
+                else:
+                    if (flag == 0):
+                        final.append("9")
+                    else:
+                        final.append("10")
             bemenet = bemenet[vag[1]:]
 
     elif start == ':':
         if ertekadas1.match(bemenet):
             match = ertekadas1.match(bemenet)
-            final.append("<értékadás>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 12")
+                else:
+                    final.append("13")
             bemenet = bemenet[vag[1]:]
 
     elif start == '<':
         if ertekadas2.match(bemenet):
             match = ertekadas2.match(bemenet)
-            final.append("<értékadás>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 14")
+                else:
+                    if (char == "="):
+                        final.append("15")
+                    else:
+                        final.append("16")
             bemenet = bemenet[vag[1]:]
 
     elif start == '>':
         if ertekadas3.match(bemenet):
             match = ertekadas3.match(bemenet)
-            final.append("<értékadás>")
             vag = match.span()
+            for i, char in enumerate(match.group()):
+                if (i == 0):
+                    final.append("1 17")
+                else:
+                    final.append("18")
             bemenet = bemenet[vag[1]:]
 
     else:
